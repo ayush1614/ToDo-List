77ac28b80e9ca3f0104b00511ea51461977965aa
Ayush Katiyar <ayushkatiyar0000gmail.com>
2021-11-29 01:21:13 +0530

    fixing minor issues

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const date = require(__dirname + "/date.js") ;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public")) ;
 app.set("view engine", "ejs");          // for using template hmhe yh karna hota hai
 
-var items = [];
-
+let items = [];
+let workItems = [] ;
 app.get("/", function (request, response) {
-
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day:"numeric"
-    }
-
-    var day = today.toLocaleDateString("en-US", options);   // name should be same of both variables 
-    response.render("list", { kindOfday: day, newListItems: items });        // passing the variable to our template to update
+    
+    let day =   date.getDate();                                                   // name should be same of both variables 
+    response.render("list", { listTitle: day, newListItems: items });        // passing the variable to our template to update
 });
 
-app.post("/", function (request, response) {
+app.get("/work",function(request,response){
+    response.render("list" , {listTitle: "WorkList", newListItems: workItems}) ;
+})
 
-    items.push(request.body.task);
-    response.redirect("/");
+app.post("/", function (request, response) {
+    let item  = request.body.task ; 
+    
+    if(request.body.list ==='WorkList')
+    {
+        workItems.push(item) ; 
+        response.redirect("/work") ; 
+    }
+    else
+    {
+        items.push(request.body.task);
+        response.redirect("/");
+    }
 
     // response.render("list" , {newListItem : item}) ;  // this gives error describes below arises due  to above render
 });
+
 
 app.listen(3000, function (request, response) {
     console.log("Server started at port 3000");
---
+++ date.js
@@ -0,0 +1,13 @@
+module.exports.getDate = getDate ;
+
+function getDate() {
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        //year: "numeric",
+        month: "long",
+        day: "numeric"
+    }
+    let day = today.toLocaleDateString("en-US", options);       //gives date in the given format 
+    return day;
+}
